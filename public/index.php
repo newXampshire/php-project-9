0ae70c8614d9862903974703980323a69383b658
New Xampshire <gun19sinking@ya.ru>
2024-09-04 20:46:20 +0300

    add base templates

--- public/index.php
+++ public/index.php
@@ -3,14 +3,20 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$twig = Twig::create(__DIR__ . '/../src/templates');
+
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello there!");
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig');
 });
 
 $app->run();
